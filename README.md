383e475b7cf923659cb3649af6c768ed934c1423
harsh adwani <harryadwani9@gmail.com>
2023-05-07 23:38:29 +0530

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-# harryadwani.github.io
--- html/nav.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("open").style.display = "block";
-    document.getElementById("nav").style.width = "100%";
-}
-function closeNav() {
-    
-    document.getElementById("nav").style.width = "0";
-    document.getElementById("open").style.display = "block";
-}
